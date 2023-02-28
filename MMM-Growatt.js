d5feb7f1ab63cc2cedba32a57d3c6c5004f3e86d
dekinet <108780459+dekinet@users.noreply.github.com>
2023-03-01 07:57:35 +1000

    Add files via upload

---
+++ MMM-Growatt.js
@@ -0,0 +1,108 @@
+Module.register("MMM-Growatt", {
+  // Default configs
+  defaults: {
+    intervalSecs: 300,
+  },
+
+  start: function () {
+    Log.log('Starting module: ' + this.name);
+    me = this;
+    this.result = null;
+
+    me.getData();
+
+    setInterval(function() {
+      me.getData(me);
+    }, me.config.intervalSecs * 1000);
+  },
+
+  getData: function (me = this) {
+    Log.info(me.name + ': Getting data');
+    me.sendSocketNotification("GROWATT_GET_DATA", {
+      config: me.config,
+    });
+  },
+
+  getStyles: function () {
+    return [
+      this.file('styles.css'),
+    ];
+  },
+
+  socketNotificationReceived: function(notification, payload) {
+    if (notification === "GROWATT_GOT_DATA") {
+      Log.info('GROWATT_GOT_DATA: ' + JSON.stringify(payload));
+      this.result = { payload: JSON.parse(payload) };
+      this.updateDom();
+    }
+  },
+
+  // Override the DOM generator
+  getDom: function () {
+    let wrapper = document.createElement('div');
+    wrapper.setAttribute('class', 'growatt-data');
+    wrapper.innerText = 'Growatt';
+    let table = document.createElement('table');
+    table.setAttribute('class', 'growatt-table');
+    wrapper.appendChild(table);
+
+    let tr = document.createElement('tr');
+    tr.setAttribute('class', 'growatt-row');
+
+    if (this.result) {
+      const mydata = this.result.payload[0].data.devicesData[0].data;
+
+      let td = document.createElement('td');
+      td.setAttribute('class', 'growatt-cell');
+      td.innerText = 'Updated:';
+      tr.appendChild(td);
+
+      td = document.createElement('td');
+      td.setAttribute('class', 'growatt-cell');
+      if (this.result.payload) {
+        td.innerText = mydata.deviceData.lastUpdateTime.split(' ')[1];
+      } else {
+        td.innerText = 'Pending';
+      }
+      tr.appendChild(td);
+      table.appendChild(tr);
+
+      tr = document.createElement('tr');
+      tr.setAttribute('class', 'growatt-row');
+
+      td = document.createElement('td');
+      td.setAttribute('class', 'growatt-cell');
+      td.innerText = 'Current:';
+      tr.appendChild(td);
+
+      td = document.createElement('td');
+      td.setAttribute('class', 'growatt-cell');
+      td.innerText = mydata.deviceData.pac + ' W';
+      tr.appendChild(td);
+
+      table.appendChild(tr);
+
+      tr = document.createElement('tr');
+      tr.setAttribute('class', 'growatt-row');
+
+      td = document.createElement('td');
+      td.setAttribute('class', 'growatt-cell');
+      td.innerText = 'Today:';
+      tr.appendChild(td);
+
+      td = document.createElement('td');
+      td.setAttribute('class', 'growatt-cell');
+      td.innerText = mydata.totalData.eToday + ' kWh';
+      tr.appendChild(td);
+    } else {
+      let td = document.createElement('td');
+      td.setAttribute('class', 'growatt-cell');
+      td.innerText = 'Pending...';
+      tr.appendChild(td);
+    }
+    table.appendChild(tr);
+
+    return wrapper;
+  },
+});
+
---
+++ node_helper.js
@@ -0,0 +1,93 @@
+const NodeHelper = require('node_helper');
+const api = require('growatt');
+
+const getJSONCircularReplacer = () => {
+  const seen = new WeakMap();
+  return (key, val) => {
+    const value = val;
+    if (typeof value === 'object' && value !== null) {
+      if (seen.has(value)) {
+        return `loop on ${seen.get(value)}`;
+      }
+      seen.set(value, key);
+    }
+    return value;
+  };
+};
+
+module.exports = NodeHelper.create({
+
+  start: function() {
+    console.log("Starting node helper: " + this.name);
+  },
+
+  socketNotificationReceived: async function(notification, payload) {
+    let self = this;
+    const options = { weather: false, historyLast: false };
+
+    if (notification === "GROWATT_GET_DATA") {
+      console.log(self.name + ': GROWATT_GET_DATA');
+
+      const growatt = new api({});
+      let data = [];
+      let login = await growatt.login(payload.config.user, payload.config.password).catch(e => {console.log(e)});
+      let getAllPlantData = await growatt.getAllPlantData(options).catch(e => {console.log(e)});
+      if (getAllPlantData) {
+        const keys = Object.keys(getAllPlantData);
+        keys.forEach(key => {
+          let { devices, ...rest } = getAllPlantData[key];
+          const serialNumbers = Object.keys(devices);
+          let devicesData = [];
+          serialNumbers.forEach(sn => {
+            devicesData.push({
+              sn: sn,
+              data: devices[sn],
+            });
+          });
+          data.push({
+            plantid: key,
+            data: { ...rest, devicesData }
+          })
+        });
+        
+//        console.log(this.name + " GOT DATA", JSON.stringify(self, getJSONCircularReplacer(), 2));
+        console.log('Publishing GROWATT data');
+        self.sendSocketNotification("GROWATT_GOT_DATA", JSON.stringify(data));
+        await growatt.logout().catch(e => {console.log(e)});
+      }
+
+/*
+      growatt
+        .login(payload.config.user, payload.config.password)
+        .then(result => {
+          return growatt.getAllPlantData(options);
+        })
+        .then(result => {
+          const keys = Object.keys(result);
+          keys.forEach(key => {
+              let { devices, ...rest } = result[key];
+              const serialNumbers = Object.keys(devices);
+              let devicesData = [];
+              serialNumbers.forEach(sn => {
+                  devicesData.push({
+                      sn: sn,
+                      data: devices[sn],
+                  });
+              });
+              data.push({
+                  plantid: key,
+                  data: { ...rest, devicesData }
+              })
+          });
+        })
+        .catch(e => { console.log('ERROR'); console.log(e)} )
+        .finally(() => {
+          console.log("GROWATT GOT DATA: " + JSON.stringify(data));
+console.log(JSON.stringify(self, null, 2));
+          this.sendSocketNotification("GROWATT_GOT_DATA", JSON.stringify(data));
+          growatt.logout()
+      });
+*/
+    }
+  }
+});
